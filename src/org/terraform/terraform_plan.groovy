9112091692d5dc59a8c7a4567cc50472002d4fa9
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-01-15 19:37:02 +0530

    Create terraform_plan.groovy

---
+++ src/org/terraform/terraform_plan.groovy
@@ -0,0 +1,10 @@
+package org.terraform
+
+class terraform_plan {
+    static void planTerraform(def steps, String dir = '.') { 
+        steps.sh """
+        cd ${dir}
+        terraform plan
+        """
+    }
+}
