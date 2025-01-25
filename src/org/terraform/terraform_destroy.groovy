95c2a13e5fd1ad136e8844bc7d887c1563da7c4b
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-01-26 00:56:12 +0530

    Update terraform_destroy.groovy

--- src/org/terraform/terraform_destroy.groovy
+++ src/org/terraform/terraform_destroy.groovy
@@ -4,7 +4,7 @@ class terraform_destroy {
     static void destroyTerraform(def steps, String dir = '.', boolean autoApprove = true) { 
         steps.sh """
         cd ${dir}
-        terraform destroy -var-file="dev.tfvars" ${autoApprove ? "-auto-approve" : ""}
+        terraform destroy  ${autoApprove ? "-auto-approve" : ""}
         """
     }
 }
