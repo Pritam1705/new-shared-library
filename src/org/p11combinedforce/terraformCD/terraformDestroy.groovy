e072fff26e100b8b8b60f6b450d70684dc60036e
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-12 21:21:45 +0530

    Create terraformDestroy.groovy

---
+++ src/org/p11combinedforce/terraformCD/terraformDestroy.groovy
@@ -0,0 +1,32 @@
+package org.p11combinedforce.terraformCD
+
+def call(String Directory) {
+    stage("Terraform apply") {
+        script {
+            def userApproval = input(
+                message: "Do you want to proceed with Terraform destroy?",
+                parameters: [
+                    choice(name: "CONFIRM", choices: ["Yes", "No"], description: "Select 'Yes' to destroy resources")
+                ]
+            )
+
+            
+            def currentUser = env.BUILD_USER_ID ?: 'unknown'
+
+            def adminUsers = ["admin"] 
+
+            if (userApproval == "Yes") {
+                if (adminUsers.contains(currentUser)) {
+                    sh """
+                        cd ${Directory}
+                        terraform destroy -auto-approve 
+                    """
+                } else {
+                    error("Unauthorized! Only Admin Users can run Terraform Destroy.")
+                }
+            } else {
+                echo "Terraform destroy was skipped by the user."
+            }
+        }
+    }
+}
